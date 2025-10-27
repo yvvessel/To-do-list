9931ea5d38760a3c41311832d25f72d7300d7597
Thiago Dos Santos Gomes <thiago.dossantosgomes032006@gmail.com>
2025-10-27 18:28:40 +0000

    Adicionando structure para todo.c

---
+++ src/todo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+    struct Alltask {
+        char taskName[30];
+        int taskStatus;
+        char taskData[30];
+        char taskInfo[30];
+        bool taskCompleted;
+        char taskCategory[30];
+    };
+
+}
