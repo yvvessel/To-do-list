ef536c043059dcf516c43146927896b517d63015
Thiago Dos Santos Gomes <thiago.dossantosgomes032006@gmail.com>
2025-10-27 21:42:24 +0000

    Adicionado remoção e adicionar conclusão das tarefas

--- src/todo.c
+++ src/todo.c
@@ -16,20 +16,42 @@ struct Task {
 void addTask(struct Task taskArray[], int *total) {
 printf("Digite o nome da tarefa: ");
 fgets(taskArray[*total].taskName, 30, stdin);
+
 printf("Qual o prazo de conclusão de sua tarefa: ");
 fgets(taskArray[*total].taskData, 30, stdin);
+
 printf("Descreva a sua tarefa ou objetivos dela: ");
 fgets(taskArray[*total].taskInfo, 30, stdin);
+
 printf("Digite a categoria de sua tarefa: ");
 fgets(taskArray[*total].taskCategory, 30, stdin);
+
 printf("Digite o status: ");
 scanf("%d", &taskArray[*total].taskStatus);
+
 taskArray[*total].taskCompleted = false;
 (*total)++;
 }
 
 void concludedTask(struct Task taskArray[], int total) {
+int indice;
+//printando a lista de tarefas e seus indices
+for(int i = 0; i < total; i++) {
+    printf("Indice: [%d] ", i);
+    printf("[%d] %s\n", i, taskArray[i].taskName);
+}
+//requisitando o indice da tarefa a ser concluida
+printf("Digite o indice da tarefa a ser concluida: ");
+scanf("%d", &indice);
+//verificando se o indice é válido
+while (indice < 0 || indice >= total) {
+    printf("Indice invalido. Digite novamente: ");
+    scanf("%d", &indice);
+}
 
+taskArray[indice].taskCompleted = true;
+
+printf("Tarefa '%s' marcada como concluida.\n", taskArray[indice].taskName);
 }
 
 void list_category(struct Task taskArray[], int total) {
@@ -37,6 +59,24 @@ void list_category(struct Task taskArray[], int total) {
 }
 
 void removeTask(struct Task taskArray[], int *total) {
+int indice;
+
+//previne o usuario de colocar valores invalidos
+while (indice < 0 || indice >= *total) {
+//printar o indice da tarefa e seu nome
+for(int i = 0; i < *total; i++) {
+    printf("Indice: [%d] ", i);
+    printf("[%d] %s\n", i, taskArray[i].taskName);
+    printf("[%d] %s\n", i, taskArray[i].taskData);
+}
+printf("Digite o indice da tarefa a ser removida: ");
+scanf("%d", &indice);
+}
+//ajustar a ordem das tarefas no array
+for (int i = indice; i < *total - 1; i++) {
+    taskArray[i] = taskArray[i+1];
+}
+//remover a tarefa
 (*total)--;
 }
 
