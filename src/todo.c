60919d20edce964cb984b9a2a8dce1200d1abe4e
Thiago Dos Santos Gomes <thiago.dossantosgomes032006@gmail.com>
2025-10-27 19:22:54 +0000

    Adicionando estrutura das chamadas do menu

--- src/todo.c
+++ src/todo.c
@@ -9,26 +9,57 @@ struct Task {
         char taskCategory[30];
     };
 
-void addTask(struct Task taskArray[], int total) {
-
+void addTask(struct Task taskArray[], int *total) {
+(*total)++;
 }
 
 void concludedTask(struct Task taskArray[], int total) {
 
 }
 
-void list_category(struct Task taskArray[], int *total) {
-    
+void list_category(struct Task taskArray[], int total) {
+
 }
 
 void removeTask(struct Task taskArray[], int *total) {
-
+(*total)--;
 }
 
 int main() {
 
     struct Task taskArray[100];
-
     int totalTasks = 0;
+    int menuOption;
+
+    while (true) {
+        printf("1 - Adicionar tarefa\n2 - Listar tarefas\n3 - Marcar como concluído\n4 - Remover tarefa\n5 - Salvar tarefas\n6 - Sair");
+        scanf("%d", &menuOption);
+
+        switch(menuOption) {
+            case 1:
+            addTask(taskArray, &totalTasks);
+            break;
+
+            case 2:
+            list_category(taskArray, &totalTasks);
+            break;
+            
+            case 3:
+            concludedTask(taskArray, &totalTasks);
+            break;
+
+            case 4:
+            removeTask(taskArray, &totalTasks);
+            break;
+
+            case 5:
+            
+        }
+        
+        if(menuOption == 6) {
+            printf("Saindo...\n");
+            break;
+        }
+    }
 
 }
